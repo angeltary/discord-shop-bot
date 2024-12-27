97e10b47f2000d05e9bd6f0676d45342abfd6ec7
Yaroslav <fishatary@proton.me>
2024-12-27 14:08:53 +0300

    Deleting cart if user closed ticket

--- src/buttons/closeTicket.ts
+++ src/buttons/closeTicket.ts
@@ -1,4 +1,5 @@
 import { TextChannel } from 'discord.js'
+import { cartService } from '../services/cart.service'
 import Button from '../structures/Button'
 
 export default new Button('close-ticket', async (interaction) => {
@@ -9,6 +10,11 @@ export default new Button('close-ticket', async (interaction) => {
       content: 'This is not a ticket channel!',
       ephemeral: true
     })
+  }
+
+  const cart = cartService.getCart(interaction.user.id)
+  if (cart.length) {
+    cartService.deleteCart(interaction.user.id)
   }
 
   channel.delete()
